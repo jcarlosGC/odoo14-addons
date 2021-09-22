88c86d1d8d3091abb3b0cea2cf2881b2e6b7ba35
jcarlosGC <74275389+jcarlosGC@users.noreply.github.com>
2021-09-23 00:16:06 +0100

    Add files via upload

--- modulo_1/__manifest__.py
+++ modulo_1/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['sale.order'],
+    'depends': ['sale'],
 
     # always loaded
     'data': [ ],
